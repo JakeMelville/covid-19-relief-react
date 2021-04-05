915c36f1f8f01978ec8070fad0f0a30301b760e5
Jake Melville <jakemelville@ymail.com>
2021-04-05 14:29:03 -0400

    created components folder- header, nav, covidupdate, testfinder, and vaccinefinder js files created'

---
+++ src/components/CovidUpateSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CovidUpdate() {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+export default CovidUpdate;
---
+++ src/components/TestFinder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TestFinder() {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+export default TestFinder;
