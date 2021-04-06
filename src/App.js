af15f8f6ddb2cd67c40f7a67d79ccf70a8ab778d
Jake Melville <jakemelville@ymail.com>
2021-04-06 10:03:33 -0400

    added routes to app.js. Imported react-router-dom and axios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header/index';
 import Nav from './components/Nav/index'
 import CovidUpdate from './components/CovidUpdateSection/index';
 import TestFinder from './components/TestFinder/index';
 import VaccineFinder from './components/VaccineFinder/index.js';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import MyProfile from './pages/MyProfile'
+import Wrapper from './components/Wrapper/index'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <CovidUpdate />
-      <TestFinder />
-      <VaccineFinder />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Nav />
+        <CovidUpdate />
+        <TestFinder />
+        <VaccineFinder />
+        <Wrapper>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/myprofile' component={MyProfile} />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header/index';
+
+function Home () {
+    return (
+        <h1>home</h1>
+    )
+}
+
+export default Home;
