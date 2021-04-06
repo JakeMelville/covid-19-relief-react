1aae76b7535d4c37c724b2bef2c12e44c263a1fd
Jake Melville <jakemelville@ymail.com>
2021-04-06 12:15:26 -0400

    created API.js file, added query strings for mapquest api and discover api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+export default {
+    getLocation: function() {
+        return axios.get(`https://open.mapquestapi.com/staticmap/v5/map?key=${ApiKey}&location=${locationInput}`)
+    },
+    getTestingSites: function() {
+        return axios.get(`https://discover.search.hereapi.com/v1/discover?apikey=${discoverApiKey}&q=Covid&at=${locationLat},${locationLng}&limit=5`)
+    }
+
+}
