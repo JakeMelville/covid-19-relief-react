b69c2017bac3260daf800020eb5e94250a341622
Jake Melville <jakemelville@ymail.com>
2021-04-06 11:03:59 -0400

    navbar now fully functional, all the links go to the correct page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css'
 
 function Nav() {
@@ -10,20 +11,65 @@ function Nav() {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Login <span class="sr-only">(current)</span></a>
+                        <li class="nav-item">
+                            <Link 
+                                to='/login'
+                                className={
+                                    window.location.pathname === '/login'
+                                    ? 'nav-link active'
+                                    : 'nav-link'
+                                }
+                                >
+                                    Login
+                            </Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sign Up</a>
+                            <Link 
+                                to='/signup'
+                                className={
+                                    window.location.pathname === '/signup'
+                                    ? 'nav-link active'
+                                    : 'nav-link'
+                                }
+                                >
+                                    Sign Up
+                            </Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Test Locator</a>
+                            <Link 
+                                to='/testlocator'
+                                className={
+                                    window.location.pathname === '/testlocator'
+                                    ? 'nav-link active'
+                                    : 'nav-link'
+                                }
+                                >
+                                    Test Locator
+                            </Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Vaccine Finder</a>
+                            <Link 
+                                to='/vaccinefinder'
+                                className={
+                                    window.location.pathname === '/vaccinefinder'
+                                    ? 'nav-link active'
+                                    : 'nav-link'
+                                }
+                                >
+                                    Vaccine Finder
+                            </Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">My Profile</a>
+                            <Link 
+                                to='/myprofile'
+                                className={
+                                    window.location.pathname === '/myprofile'
+                                    ? 'nav-link active'
+                                    : 'nav-link'
+                                }
+                                >
+                                    My Profile
+                            </Link>
                         </li>
                     </ul>
                 </div>
