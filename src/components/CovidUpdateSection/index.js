2f54eefb827973a97442a64cbd4b646a33eddaae
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-04-07 13:43:01 -0400

    updating CovidUpdateSection

--- src/components/CovidUpdateSection/index.js
+++ src/components/CovidUpdateSection/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
+
 function CovidUpdate() {
-    <iframe src="https://www.nytimes.com/2021/04/05/us/coronavirus-today-vaccines.html"
-    scrolling="no" height="285" frameborder="0" width="300" title="Vaccine Finder Widget"
-    style="overflow: hidden" frameborder="0">
-    </iframe>
+    const [CovidUpdateState, setCovidUpdateState] = useState({})
+
+    const baseURL = 'https://api.nytimes.com/svc/topstories/v2/health.json?&api-key=hAIQqDrmbcaMwVbXpvGgflMSyUEYnZ4D'
+    //https://api.nytimes.com/svc/topstories/v2/health.json?&api-key=hAIQqDrmbcaMwVbXpvGgflMSyUEYnZ4D
+    //'https://api.nytimes.com/svc/topstories/v2/us.json?api-key=hAIQqDrmbcaMwVbXpvGgflMSyUEYnZ4D'
+    //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid&api-key=hAIQqDrmbcaMwVbXpvGgflMSyUEYnZ4D
+    //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coronavirus+in+the+us&api-key=hAIQqDrmbcaMwVbXpvGgflMSyUEYnZ4D
+    
+            axios.get(baseURL)
+            .then(res => {
+                console.log() 
+                console.log(res)
+
+                setCovidUpdateState(res.data.results[10])
+
+            }
+            )
+            return (
+                <div className="card">
+                    <p>testing</p>
+                    <p>{CovidUpdateState.title}</p>
+                    <p>{CovidUpdateState.abstract}</p>
+                </div>
+            ) 
+
+            
+    //return Axios.get(`${config.baseURL}`, {headers: config.headers}).then(res => {
+        //return res.results[0].section[0].title['Coronavirus Briefing: What Happened Today'];
     
 }
 
