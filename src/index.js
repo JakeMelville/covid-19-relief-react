fdf2b812b132036b1f58e90b52a37b6713076db7
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-07 19:01:10 -0400

    updating index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from "./Auth/auth";
 
 
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+ReactDOM.render(
+  <Router>
+    <Auth>
+      <App />
+    </Auth>
+  </Router>,
+  document.getElementById("root")
+);
