dca281c49a225783c3467a13a4cf72c54df43233
Jake Melville <jakemelville@ymail.com>
2021-04-07 17:12:10 -0400

    worked on testlocator.js, still having issues saving the input value

--- src/components/TestFinder/index.js
+++ src/components/TestFinder/index.js
@@ -10,7 +10,7 @@ function TestFinder(props) {
                         <div className="row">
                             <h2 className="col-12 text-center">Find Testing Centers Near You</h2>
                             <form className="col-12 text-center pb-3">
-                                <input id="input" type="text" placeholder="City, State" value={props.locationState}  onKeyPress={props.handleSubmit}/>
+                                <input id="input" type="text" placeholder="City, State" value={props.locationState} onKeyPress={props.handleKeyPress}/>
                                 <button id="submitBtn" className='btn-primary'>Submit</button>
                             </form>
                         </div>
--- src/pages/TestLocator.js
+++ src/pages/TestLocator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TestFinder from '../components/TestFinder/index';
 import Nav from '../components/Nav/index';
 import API from '../utils/API';
@@ -15,43 +15,49 @@ function TestLocator() {
   const [mapSrc, setMapSrc] = useState('')
 
 
-  API.getLatLng(locationState)
-    .then(res => {
-      console.log('line 20 location', locationState);
-      console.log(res.data);
-      setLatState(res.data.results[0].locations[0].displayLatLng.lat);
-      setLngState(res.data.results[0].locations[0].displayLatLng.lng);
+  const handleSubmit = (event) => {
+    const inputRef = useRef();
 
-
-      API.getTestingSites(latState, lngState)
-        .then(res => {
-          // console.log(res.data);  
-          setTestingSite1('1. ' + res.data.items[0].address.label);
-          setTestingSite2('2. ' + res.data.items[1].address.label);
-          setTestingSite3('3. ' + res.data.items[2].address.label);
-          setTestingSite4('4. ' + res.data.items[3].address.label);
-          setTestingSite5('5. ' + res.data.items[4].address.label);
-          setMapSrc(`https://open.mapquestapi.com/staticmap/v5/map?key=WFU3qGtFSrPpKOpC8vhpd3bfXKHEP4My&banner=Covid+19+Test+Sights|top&size=400,400&zoom=10&locations=${res.data.items[0].access[0].lat},${res.data.items[0].access[0].lng}|marker-1||${res.data.items[1].access[0].lat},${res.data.items[1].access[0].lng}|marker-2||${res.data.items[2].access[0].lat},${res.data.items[2].access[0].lng}|marker-3||${res.data.items[3].access[0].lat},${res.data.items[3].access[0].lng}|marker-4||${res.data.items[4].access[0].lat},${res.data.items[4].access[0].lng}|marker-5||`)
-        })
-    })
-    .catch(err => console.log('line 37', err));
-
-  const handleKeyPress = (event) => {
+    console.log('handlesubmit runs');
     event.preventDefault();
-    if (event.key === 'Enter') {
-      setLocationState(event.target.value)
-      console.log('loc state', locationState);
+    setLocationState(event.target.value)
+    console.log(locationState);
 
+    if (locationState) {
+
+      API.getLatLng(locationState)
+        .then(res => {
+          console.log('line 20 location', locationState);
+          console.log(res.data);
+          setLatState(res.data.results[0].locations[0].displayLatLng.lat);
+          setLngState(res.data.results[0].locations[0].displayLatLng.lng);
+
+
+          API.getTestingSites(latState, lngState)
+            .then(res => {
+              // console.log(res.data);  
+              setTestingSite1('1. ' + res.data.items[0].address.label);
+              setTestingSite2('2. ' + res.data.items[1].address.label);
+              setTestingSite3('3. ' + res.data.items[2].address.label);
+              setTestingSite4('4. ' + res.data.items[3].address.label);
+              setTestingSite5('5. ' + res.data.items[4].address.label);
+              setMapSrc(`https://open.mapquestapi.com/staticmap/v5/map?key=WFU3qGtFSrPpKOpC8vhpd3bfXKHEP4My&banner=Covid+19+Test+Sights|top&size=400,400&zoom=10&locations=${res.data.items[0].access[0].lat},${res.data.items[0].access[0].lng}|marker-1||${res.data.items[1].access[0].lat},${res.data.items[1].access[0].lng}|marker-2||${res.data.items[2].access[0].lat},${res.data.items[2].access[0].lng}|marker-3||${res.data.items[3].access[0].lat},${res.data.items[3].access[0].lng}|marker-4||${res.data.items[4].access[0].lat},${res.data.items[4].access[0].lng}|marker-5||`)
+            })
+        })
+        .catch(err => console.log('line 37', err));
+    }
+    else {
+      console.log('still nothing');
     }
   }
 
   return (
     <div>
       <Nav />
-      <TestFinder
-        handleKeyPress={handleKeyPress}
-        results={locationState}
-      />
+      <form className="col-12 text-center pb-3" onSubmit={handleSubmit}>
+        <input id="input" type="text" placeholder="City, State" value={locationState}/>
+        <button id="submitBtn" className='btn-primary' type='submit'>Submit</button>
+      </form>
       <div className='row text-left pl-5 justify-content-center'>
         <ul className='col-4'>
           <li className='p-2'>{testingSite1}</li>
@@ -63,8 +69,9 @@ function TestLocator() {
         <img className='col-4' src={mapSrc} alt='map' />
       </div>
     </div>
-  )
-}
+  );
+};
+
 
 
 export default TestLocator;
