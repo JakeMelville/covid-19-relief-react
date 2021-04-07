dad60898aa392e99236b5f000637b2b51a01f819
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-07 19:37:39 -0400

    saving logout.js and adding auth.js

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,1 +1,20 @@
-import React 
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Logout = () => {
+    const { logout } = useAuth0();
+    return ( 
+        <button
+        className="btn btn-danger btn-block"
+        onClick={() =>
+        logout({
+            returnTo: window.location.origin,
+        })
+    }
+    >
+        Log out
+    </button>
+    );
+};
+
+export default Logout;
