9a629956dff5515612dbf37096f0adf416a9702b
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-08 11:04:45 -0400

    saving

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,29 +1,12 @@
-// import React from 'react';
-// import Nav from '../components/Nav/index';
+import React from 'react';
+import Nav from '../components/Nav/index';
 
-// function MyProfile() {
-//     return (
-//         <div>
-//             <Nav />
-//         </div>
-//     )
-// }
-
-// export default MyProfile;
-
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-
-const MyProfile = () => {
-    const { user } = useAuth0();
-
+function MyProfile() {
     return (
-            <div>
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-                {JSON.stringify(user)}
-            </div>
-    );
-};
+        <div>
+            <Nav />
+        </div>
+    )
+}
 
 export default MyProfile;
