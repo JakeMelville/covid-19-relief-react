c113645b930c1154b99dd3029d71d073b17feb96
Jake Melville <jakemelville@ymail.com>
2021-04-08 12:32:30 -0400

    worked on testLocator

--- src/components/TestFinder/index.js
+++ src/components/TestFinder/index.js
@@ -8,9 +8,9 @@ function TestFinder(props) {
                 <div className="container w-100 m-0 p-">
                     <div className="row w-100 m-0">
                         <h2 className="col-12 text-center">Find Testing Centers Near You</h2>
-                        <form className="col-12 text-center pb-3" onSubmit={props.handleSubmit}>
+                        <form id="inputForm"className="col-12 text-center pb-3" onSubmit={props.handleSubmit}>
                             <input id="input" type="text" placeholder="City, State" value={props.locationState} onChange={(e) => props.setLocationState(e.target.value)} />
-                            <button id="submitBtn" className='btn-primary'>Submit</button>
+                            <button id="submitBtn" className='btn-primary' type='submit'>Submit</button>
                         </form>
                     </div>
                 </div>
--- src/pages/TestLocator.js
+++ src/pages/TestLocator.js
@@ -26,10 +26,10 @@ function TestLocator() {
   const[number5, setNumber5] = useState('')
 
 
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
     console.log('handlesubmit runs');
-    console.log(locationState);
+    // console.log(locationState);
 
     API.getLatLng(locationState)
       .then(res => {
@@ -42,26 +42,26 @@ function TestLocator() {
   }
   API.getTestingSites(latLngState)
     .then(res => {
-      console.log(res.data);  
+      // console.log(res.data);  
       setTestingSite1('1. ' + res.data.items[0].address.label);
-      setLink1(res.data.items[0].contacts[0].www[0].value)
-      setNumber1(res.data.items[0].contacts[0].phone[0].value)
+      setLink1(res.data.items[0].contacts[0].www[0].value);
+      setNumber1(res.data.items[0].contacts[0].phone[0].value);
 
       setTestingSite2('2. ' + res.data.items[1].address.label);
-      setLink2(res.data.items[1].contacts[0].www[0].value)
-      setNumber1(res.data.items[0].contacts[0].phone[0].value)
+      setLink2(res.data.items[1].contacts[0].www[0].value);
+      setNumber1(res.data.items[1].contacts[0].phone[0].value);
 
       setTestingSite3('3. ' + res.data.items[2].address.label);
-      setLink3(res.data.items[2].contacts[0].www[0].value)
-      setNumber1(res.data.items[0].contacts[0].phone[0].value) 
+      setLink3(res.data.items[2].contacts[0].www[0].value);
+      setNumber1(res.data.items[2].contacts[0].phone[0].value); 
 
       setTestingSite4('4. ' + res.data.items[3].address.label);
-      setLink4(res.data.items[3].contacts[0].www[0].value)
-      setNumber1(res.data.items[0].contacts[0].phone[0].value)
+      setLink4(res.data.items[3].contacts[0].www[0].value);
+      setNumber1(res.data.items[3].contacts[0].phone[0].value);
 
       setTestingSite5('5. ' + res.data.items[4].address.label);
-      setLink5(res.data.items[4].contacts[0].www[0].value)
-      setNumber1(res.data.items[0].contacts[0].phone[0].value)
+      setLink5(res.data.items[4].contacts[0].www[0].value);
+      setNumber1(res.data.items[4].contacts[0].phone[0].value);
 
       setMapSrc(`https://open.mapquestapi.com/staticmap/v5/map?key=WFU3qGtFSrPpKOpC8vhpd3bfXKHEP4My&banner=Covid+19+Test+Sights|top&size=400,400&zoom=10&locations=${res.data.items[0].access[0].lat},${res.data.items[0].access[0].lng}|marker-1||${res.data.items[1].access[0].lat},${res.data.items[1].access[0].lng}|marker-2||${res.data.items[2].access[0].lat},${res.data.items[2].access[0].lng}|marker-3||${res.data.items[3].access[0].lat},${res.data.items[3].access[0].lng}|marker-4||${res.data.items[4].access[0].lat},${res.data.items[4].access[0].lng}|marker-5||`)
     })
