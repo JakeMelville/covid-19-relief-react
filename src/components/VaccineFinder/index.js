4dba5a515e0c3214f16b348552b0751082f1ddfe
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-04-08 15:13:17 -0400

    for BCS

--- src/components/VaccineFinder/index.js
+++ src/components/VaccineFinder/index.js
@@ -4,20 +4,20 @@ import Iframe from 'react-iframe'
 
 function VaccineFinderWidget() {
     return (
-        <div>
-            <p>
+        <div class="card mx-auto w-auto h-auto border-0 align-self-center">
+            <p class="">
         <a href={"https://www.nytimes.com/interactive/2020/us/covid-19-vaccine-doses.html"} target="_blank">For latest COVID-19 Vaccination Updates</a>
         </p>
  
 
-        <Iframe url="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/vaccine-finder-widget.html" target="_blank"
-        position="absolute"
-        width="50%"
+        <Iframe id="widget" url="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/vaccine-finder-widget.html" target="_blank"
+        position="relative"
+        width="288px"
         id="myId"
         className="myClassname"
-        height="50%"
-        styles={{height: "25px"}}
-        border="0"/>
+        height="281px"
+        margin-bottom="0"
+        />
 
 
         
