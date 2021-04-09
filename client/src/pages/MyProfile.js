72eae3ff10a4a93b13272879a89319d091ac0730
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-09 02:54:44 -0400

    saving MyProfile.js, still need to add more

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -3,12 +3,32 @@ import Nav from '../components/Nav/index';
 import MyProfile from "../components/MyProfile/myProfile";
 import axios from "axios";
 
-function MyProfile() {
-    return (
-        <div>
-            <Nav />
-        </div>
-    )
+class MyProfile extends React.Component {
+    state = {
+        loggedIn: localStorage.getItem("loggedIn"),
+        userID: "",
+
+        name: "",
+        cellPhone: "",
+        email: "",
+        password: "",
+
+        visible: true,
+    }
+
+    patientInfo = (patientData) => {
+        axios.get(`/pateint/${patientData}`)
+        .then((res) => {
+            this.state({
+                name: res.data[0].name,
+                cellPhone: res.data[0].cellPhone,
+                email: res.data[0].email,
+            });
+        })
+        .catch((err) => {
+            console.log(error);
+        });
+    }
 }
 
 
