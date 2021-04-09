48f9f519210e6eb17ef8be25b1f97c6233236abe
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-09 02:47:25 -0400

    adding myProfile.js

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Nav from '../components/Nav/index';
+import MyProfile from "../components/MyProfile/myProfile";
+import axios from "axios";
 
 function MyProfile() {
     return (
@@ -9,21 +11,5 @@ function MyProfile() {
     )
 }
 
-// export default MyProfile;
-
-// import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// const MyProfile = () => {
-//     const { user } = useAuth0();
-
-//     return (
-//             <div>
-//                 <h2>{user.name}</h2>
-//                 <p>{user.email}</p>
-//                 {JSON.stringify(user)}
-//             </div>
-//     );
-// };
 
 export default MyProfile;
