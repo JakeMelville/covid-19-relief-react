24bee3c2568fba42947c13cc551b974abd1aa9dd
Jake Melville <jakemelville@ymail.com>
2021-04-12 11:57:58 -0400

    creaded a form with dropdown options of all states

--- client/src/pages/VaccineFinder.js
+++ client/src/pages/VaccineFinder.js
@@ -1,29 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import Nav from '../components/Nav/index';
 import VaccineFinderWidget from '../components/VaccineFinder';
+import VaccineSearch from '../components/VaccineSearch'
 import API from '../utils/API';
 
 function VaccineFinder() {
     const [vaccineState, setVaccineState] = useState('')
 
-    
 
 
-    API.getVaccineSites()
-        .then(res => {
-            console.log(res);
+    function handleSubmit(event) {
+        event.preventDefault();
 
 
-        })
+        API.getVaccineSites()
+            .then(res => {
+                console.log(res.data);
+
+                switch (vaccineState) {
+                    case 'AL':
+                        return console.log(res.data[0]);
+                }
+            
+            })
+    }
 
 
-
-
+    console.log(vaccineState);
 
     return (
         <div>
             <Nav />
             <VaccineFinderWidget />
+            <VaccineSearch
+                vaccineState={vaccineState}
+                setVaccineState={setVaccineState}
+                handleSubmit={handleSubmit}
+            />
         </div>
     )
 }
