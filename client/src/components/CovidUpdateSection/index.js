8f2710bb0c6ebcad701751a5224a5646b333360a
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-04-13 15:22:17 -0400

    removed iframe client/src/components/CovidUpdateSection/index.js

--- client/src/components/CovidUpdateSection/index.js
+++ client/src/components/CovidUpdateSection/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Iframe from "react-iframe";
 import "../CovidUpdateSection/style.css";
 
 function CovidUpdate() {
