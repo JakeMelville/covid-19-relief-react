8239299ad598776d1085c706bc44782a82b0e146
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-13 11:45:45 -0400

    rewriting code for local in passport

--- passport/local.js
+++ passport/local.js
@@ -1,5 +1,6 @@
-const db = require("../models");
-const local = require("passport-local").Strategy;
+const PatientModel = require("../models");
+// const db = require("../models");
+// const local = require("passport-local").Strategy;
 
 // const storage = new local(
 //     {
@@ -22,3 +23,25 @@ const local = require("passport-local").Strategy;
 // );
 
 // module.exports = storage;
+
+module.exports = {
+    loginPatient: (email, password, callback) => {
+        PatientModel.findOne({ email: email }).exec((error, patient) => {
+            if (error) {
+                callback({ error: true })
+            } else if (!patients) {
+                callback({ error: true })
+            } else {
+                patient.comparePassword(password, (matchError, isMatch) => {
+                    if (matchError) {
+                        callback({ error: true })
+                    } else if (!isMatch) {
+                        callback({ error: true })
+                    } else {
+                        callback({ success: true })
+                    }
+                })
+            }
+        })
+    }
+}
