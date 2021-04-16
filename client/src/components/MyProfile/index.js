9854d7d6c79b359fedc09ce919d7915b0ec7580e
dvaghani <dhruvi.vaghani@gmail.com>
2021-04-16 13:47:37 -0400

    cleaning up

--- client/src/components/MyProfile/index.js
+++ client/src/components/MyProfile/index.js
@@ -1,11 +1,24 @@
-// import React from 'react';
-// import { Link, withRouter } from 'react-router-dom';
-// import Nav from '../components/Nav/index';
+import React from "react";
 
-// function PatientProfile() {
-//     return (
-//         <div>
-//             <Nav />
-//         </div>
-//     )
-// }
+function MyProfile() {
+  return (
+    <div className="row justify-content-center pt-5">
+      <div className="card col-8 justify-content-around">
+        <div className="card-body">
+          <h5 className="card-title">Name</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Email:</li>
+          <li className="list-group-item">Cell Phone Number:</li>
+          <li className="list-group-item">Password</li>
+        </ul>
+        <div className="card-body">
+          <a href="#" className="card-link">
+            Not sure of we'll need a link
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default MyProfile;
